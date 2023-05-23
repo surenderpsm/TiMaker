366533b8eae74cc7a1e047a17d4960bf08f05c63
Surender Soundiramourty <psm.surender@gmail.com>
2023-05-23 20:30:45 +0530

    Added Menu Bar. (not completed)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TiMaker
         {
             InitializeComponent();
         }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
